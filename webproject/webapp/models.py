9e6ce8c86e79e4288440c2dcc4c0ff4c1d5b89bf
bob <shansilla@gmail.com>
2024-01-19 11:59:02 +0530

    first commit

---
+++ webproject/webapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=250)
+    img = models.ImageField(upload_to='pic')
+    des = models.TextField()
+
+class Team(models.Model):
+    name = models.CharField(max_length=250)
+    img = models.ImageField(upload_to='pic')
+    des = models.TextField()
+
+
+    def __str__(self):
+        return self.name
